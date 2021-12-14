432ba9bf7a0bccc9530ba6a455c9822f4111f4e7
Svetlana Astapova <morgan159@mail.ru>
2021-12-15 04:44:05 +0500

    start

---
+++ src/main/java/com/marketplace/marketplace_main/MarketplaceMainApplication.java
@@ -0,0 +1,13 @@
+package com.marketplace.marketplace_main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketplaceMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketplaceMainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/marketplace/marketplace_main/MarketplaceMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marketplace.marketplace_main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplaceMainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
